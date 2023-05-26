76548a6f0802c7c9c052aa4a6d1778321c9ebc2a
Che Diaz Fadel <che.diazfadel@frazil.com>
2023-05-26 11:33:52 -0600

    added labels to bins

--- github_setup_assignment.R
+++ github_setup_assignment.R
@@ -7,4 +7,6 @@ df %>%
   ggplot() +
   geom_histogram(aes(x),
                  binwidth = 0.2,
-                 fill = "darkred", color = "white")
+                 fill = "darkred", color = "white") +
+  stat_bin(aes(x = x, y = after_stat(count), label = ifelse(after_stat(count) == 0, "", after_stat(count))),
+             geom = "text", binwidth = 0.2, size = 5, fontface = "bold", vjust = 0)
